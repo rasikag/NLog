56e46c3f516230bc82d63e93433b6e6c5e050aa3
Rasika Gayan Gunarathna <rasikagayan90@gmail.com>
2018-04-24 00:41:13 +0800

    nlog init

--- NLog_v1.0/NLog_v1.0/Program.cs
+++ NLog_v1.0/NLog_v1.0/Program.cs
@@ -8,8 +8,12 @@ namespace NLog_v1._0
 {
     class Program
     {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            logger.Error("Error {0}", "#01");
+            logger.Debug("Debug {0}", "#01");
         }
     }
 }
